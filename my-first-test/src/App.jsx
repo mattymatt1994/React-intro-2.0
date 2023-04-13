2d34de73d8db4fe4d7da85a4d400d5f26a44b56b
mattymatt1994 <jedimatt.jordan@gmail.com>
2023-04-13 17:06:34 -0400

    bleh

--- my-first-test/src/App.jsx
+++ my-first-test/src/App.jsx
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import "./App.css";
 import FilmsList from "./components/filmsList.jsx";
+import SingleFilmPage from "./components/singlefilm.page";
 class App extends Component {
   constructor(props) {
     super(props);
